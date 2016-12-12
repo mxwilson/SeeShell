67a2836cacae0d8ccf9e1814fea1ebf27e8c4cfc
mxwilson <matthewxwilson@gmail.com>
2016-12-12 14:06:07 -0500

    second

--- shell.c
+++ shell.c
@@ -5,23 +5,30 @@ License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 No warranty. Software provided as is.
 Copyright Matthew Wilson, 20116.
 */
+
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<string.h>
 
 
-int check_for_builtins(char line[1024]) {
+int check_for_builtins(char* token[99]) {
 	int x;
 	char* list[4] = {"?", "help", "yo"};
 
 	// if we hit one of the above, say ? then run an ls on builtin commands?
 	while (x < sizeof(list)/sizeof(list[0])) {
-		if (strcmp(line, list[x]) == 0) {
+		if (strcmp(token[0], list[x]) == 0) {
 			printf("found\n");
+
+			//if (strcmp(token[0], list[2]) == 0) {
+			//	if (chdir(token[1]) != 0) {
+			//		printf("no dir: %s\n", token[1]);
+			//		return(1);
+			//	}
 			return(0);
-		}		
-		x++;
+		}
+	x++;
 	}
 	return(1);
 }
@@ -29,30 +36,27 @@ int check_for_builtins(char line[1024]) {
 
 // forking
 
-child_process(char line[1024]) {
+child_process(char* token[99]) {
 	//printf("Now in the child process\n");
 
 	char bindir[1048] = "./bin/";
 	char prog_to_run[1048]; 
-	strcat(bindir, line);
+	strcat(bindir, token[0]);
 	strcpy(prog_to_run, bindir);
 	
-	printf("prog to run: %s\n", prog_to_run);
+	//printf("prog to run: %s\n", prog_to_run);
 	// run the concatenated program ie ./bin/progname. 
 	// visit 'builtins' func to check if program is not found
-
-	if (execl(prog_to_run, line, "-la", NULL) == -1) {
-
-	//if (execvp(prog_to_run, "-l") == -1) {
-		if (check_for_builtins(line) == 1) 
+	
+	if (execvp(prog_to_run, token) == -1) {
+		if (check_for_builtins(token) == 1) 
 			_exit(EXIT_FAILURE);
 	}
-	 
 	_exit(EXIT_SUCCESS);
 	
 }
 
-forker(char line[1024]) {
+forker(char* token[99]) {
 	pid_t child_pid = 0;
 
 	// returns processs id of forked child
@@ -62,7 +66,7 @@ forker(char line[1024]) {
 
 	// execte the child process
 	if (child_pid == 0) { 
-		child_process(line);
+		child_process(token);
 	}	
 	
 	else if (child_pid > 0) {	
@@ -89,26 +93,54 @@ forker(char line[1024]) {
 		
 }
 
+int arg_checker(char line[1024]) {
+	//tokenize the incoming command
+	int j, i = 0;
+	char user_input[1024];
+	char* delim = " ";
+	char* token[99];
+
+	//printf("arg checker\n");
+	
+	strcpy(user_input, line);
+	
+	token[0] = strtok(user_input, delim);
+
+	while (token[i] != NULL) {
+		i++;
+		token[i] = strtok(NULL, delim);
+	}
+	
+	// for (j = 0; j < i; j++) {
+	// 	printf("%d: %s\n", j, token[j]);
+	// }
+ 
+	
+	forker(token);
+}
+
 int prompt() {
 	char cwd[1024];
 	char line[1024];
 
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		printf("%s> ", cwd);
-	}
-	else {
-		printf("> ");
-	}
-	fgets(line, sizeof(line), stdin);
-	line[strlen(line) - 1] = '\0';
+	do {
+		if (getcwd(cwd, sizeof(cwd)) != NULL) {
+			printf("%s> ", cwd);
+		}
+		else {
+			printf("> ");
+		}
 	
-	if (strcmp(line, "exit") == 0) {
-		printf("exiting\n");
-		exit(EXIT_SUCCESS);
-	}
+		fgets(line, sizeof(line), stdin);
+		line[strlen(line) - 1] = '\0';
 	
-	forker(line);
-	
+		if (strcmp(line, "exit") == 0) {
+			printf("exiting\n");
+			exit(EXIT_SUCCESS);
+		}
+	} while ((strlen(line) < 1));
+	// proceed to arg_checker before forking
+	arg_checker(line);
 }
 
 
